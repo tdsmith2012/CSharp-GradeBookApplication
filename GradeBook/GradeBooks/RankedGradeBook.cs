0c1b8b147c1331b56f9cb44475c1dd57ca231c96
tdsmith2012 <51831559+tdsmith2012@users.noreply.github.com>
2019-07-29 11:48:32 -0600

    GradebookTypes

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,14 @@
+﻿
+using GradeBook.Enums;
+
+
+namespace GradeBook.GradeBooks
+{
+    class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name) 
+        {
+            Type = GradeBookType.Ranked;
+        }
+    }
+}
